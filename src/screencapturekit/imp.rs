a9f508110dd8e32383bbc287157f4d55932baeb0
Per Johansson <mail@perjohansson.net>
2024-11-29 15:03:04 +0100

    feat: working capture

--- src/screencapturekit/imp.rs
+++ src/screencapturekit/imp.rs
@@ -2,36 +2,45 @@ use gst::glib;
 use gst::subclass::prelude::*;
 use gst_base::prelude::*;
 use gst_base::subclass::prelude::*;
+use screencapturekit::output::sc_stream_frame_info::SCFrameStatus;
+use screencapturekit::output::sc_stream_frame_info::SCStreamFrameInfo;
+use screencapturekit::output::CMSampleBuffer;
+use screencapturekit::output::LockTrait;
+use screencapturekit::shareable_content::SCShareableContent;
+use screencapturekit::stream::configuration::pixel_format::PixelFormat;
+use screencapturekit::stream::configuration::SCStreamConfiguration;
+use screencapturekit::stream::content_filter::SCContentFilter;
+use screencapturekit::stream::output_trait::SCStreamOutputTrait;
+use screencapturekit::stream::output_type::SCStreamOutputType;
+use screencapturekit::stream::SCStream;
 
+use std::io::Read;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
 use std::sync::Mutex;
-
-#[derive(Debug)]
-struct Settings {}
-
-impl Default for Settings {
-    fn default() -> Self {
-        Settings {}
-    }
-}
 
 #[derive(Default)]
 enum State {
     #[default]
     Stopped,
-    Started {},
+    Started {
+        stream: SCStream,
+    },
 }
 
 #[derive(Default)]
 pub struct ScreenCaptureSrc {
-    settings: Mutex<Settings>,
+    receiver: Mutex<Option<Receiver<CMSampleBuffer>>>,
+    sender: Mutex<Option<Sender<CMSampleBuffer>>>,
     state: Mutex<State>,
 }
 
 use std::sync::LazyLock;
+
 static CAT: LazyLock<gst::DebugCategory> = LazyLock::new(|| {
     gst::DebugCategory::new(
         "screencapture",
-        gst::DebugColorFlags::empty(),
+        gst::DebugColorFlags::all(),
         Some("ScreenCapture Source"),
     )
 });
@@ -70,7 +79,11 @@ impl ElementImpl for ScreenCaptureSrc {
 
     fn pad_templates() -> &'static [gst::PadTemplate] {
         static PAD_TEMPLATES: LazyLock<Vec<gst::PadTemplate>> = LazyLock::new(|| {
-            let caps = gst::Caps::new_any();
+            let caps = gst_video::VideoCapsBuilder::new()
+                .format(gst_video::VideoFormat::Bgra)
+                .width(1440)
+                .height(2560)
+                .build();
             let src_pad_template = gst::PadTemplate::new(
                 "src",
                 gst::PadDirection::Src,
@@ -85,7 +98,23 @@ impl ElementImpl for ScreenCaptureSrc {
         PAD_TEMPLATES.as_ref()
     }
 }
+impl SCStreamOutputTrait for &ScreenCaptureSrc {
+    fn did_output_sample_buffer(
+        &self,
+        sample_buffer: CMSampleBuffer,
+        _of_type: SCStreamOutputType,
+    ) {
+        let info = SCStreamFrameInfo::from_sample_buffer(&sample_buffer).expect("should work");
 
+        if info.status() == SCFrameStatus::Complete {
+            let b = self.sender.lock().expect("should work");
+            if let Some(ref sender) = *b {
+                gst::info!(CAT, imp = self, "Sent");
+                sender.send(sample_buffer).ok();
+            }
+        }
+    }
+}
 impl BaseSrcImpl for ScreenCaptureSrc {
     fn is_seekable(&self) -> bool {
         false
@@ -93,14 +122,28 @@ impl BaseSrcImpl for ScreenCaptureSrc {
 
     fn start(&self) -> Result<(), gst::ErrorMessage> {
         let mut state = self.state.lock().unwrap();
-        if let State::Started { .. } = *state {
-            unreachable!("FileSrc already started");
+        if let Ok(config) = SCStreamConfiguration::new()
+            .set_width(1440)
+            .and_then(|x| x.set_height(2560))
+            .and_then(|x| x.set_pixel_format(PixelFormat::BGRA))
+        {
+            let display = SCShareableContent::get().unwrap().displays().remove(1);
+            let filter = SCContentFilter::new().with_display_excluding_windows(&display, &[]);
+            let mut stream = SCStream::new(&filter, &config);
+            stream.add_output_handler(self, SCStreamOutputType::Screen);
+
+            let (sender, receiver) = std::sync::mpsc::channel();
+
+            let mut send = self.sender.lock().expect("should work");
+            let mut recv = self.receiver.lock().expect("should work");
+
+            *send = Some(sender);
+            *recv = Some(receiver);
+
+            stream.start_capture().expect("should work");
+            *state = State::Started { stream };
         }
-
-        let settings = self.settings.lock().unwrap();
-
         gst::info!(CAT, imp = self, "Started");
-
         Ok(())
     }
 
@@ -109,8 +152,12 @@ impl BaseSrcImpl for ScreenCaptureSrc {
         if let State::Stopped = *state {
             return Err(gst::error_msg!(
                 gst::ResourceError::Settings,
-                ["FileSrc not started"]
+                ["Fil Internal data stream erroreSrc not started"]
             ));
+        }
+
+        if let State::Started { ref stream, .. } = *state {
+            stream.stop_capture().ok();
         }
 
         *state = State::Stopped;
@@ -126,7 +173,45 @@ impl BaseSrcImpl for ScreenCaptureSrc {
         _length: u32,
         buffer: &mut gst::BufferRef,
     ) -> Result<gst::FlowSuccess, gst::FlowError> {
-        let mut state = self.state.lock().unwrap();
+        let b = self.receiver.lock().expect("should work");
+        if let Some(ref receiver) = *b {
+            let b = receiver.recv().expect("should get");
+            let pb = b.get_pixel_buffer().expect("should work");
+
+            let internal_base_address = pb.lock().expect("TEST");
+            let mut curs = internal_base_address.as_cursor();
+            let size = {
+                let mut map = buffer.map_writable().map_err(|_| {
+                    gst::element_imp_error!(
+                        self,
+                        gst::LibraryError::Failed,
+                        ["Failed to map buffer"]
+                    );
+                    gst::FlowError::Error
+                })?;
+
+                curs.read(map.as_mut()).map_err(|err| {
+                    gst::element_imp_error!(
+                        self,
+                        gst::LibraryError::Failed,
+                        ["Failed to read at {}: {}", offset, err.to_string()]
+                    );
+                    gst::FlowError::Error
+                })?
+            };
+
+            gst_video::VideoMeta::add_full(
+                buffer,
+                gst_video::VideoFrameFlags::empty(),
+                gst_video::VideoFormat::Bgra,
+                pb.get_width(),
+                pb.get_height(),
+                &[0],
+                &[pb.get_bytes_per_row() as i32],
+            )
+            .ok();
+            buffer.set_size(size);
+        }
 
         Ok(gst::FlowSuccess::Ok)
     }
